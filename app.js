c33b7eb4d549c60c04f0fa085f06fda30b861353
ismiagesta321-dotcom <ismiagesta321@gmail.com>
2025-09-19 16:02:29 +0700

    baru

---
+++ app.js
@@ -0,0 +1,52 @@
+// console.log(`Listening is Listening on port ${port}..`);
+// });
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
+app.use(expressLayouts);
+app.set('layout','layouts/main-layout');
+app.get('/', (req, res)=> {
+   
+    const mahasiswa =[
+        {nama : 'Ade',
+            email : 'ade@gmail.com',
+        },
+        {nama : 'Asep',
+            email : 'asep@gmail.com',
+        },
+        {nama : 'Agus',
+            email :'agus@gmail.com',
+        }
+    ];
+
+    res.render('index',{
+        nama : 'Ade',
+        title : 'Halaman Home',
+        mahasiswa : mahasiswa,
+    });
+});
+app.get('/about',(req,res)=> {
+    res.render('about',{title: 'Halaman About'}
+        );
+   
+});
+app.get('/contact',(req,res)=> {
+    res.render('contact',{title: 'Halaman Kontak'});
+});
+   
+app.get('/product/:id',(req,res)=> {
+    res.send(`product ID : ${req.params.id} <br> Catgory ID : ${req.params.idCat}`);
+});
+app.use('/',(req,res)=>{
+    res.status(404);
+    res.send('<h1>Test</h1>');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
